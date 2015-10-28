80c6bde0820a9bc8bdb7824fb52970dbf1c20f21
Fan Jiang <fanjiang@thoughtworks.com>
2015-10-28 13:35:38 -0500

    example for user info query

--- app/api/api/users.rb
+++ app/api/api/users.rb
@@ -7,5 +7,15 @@ module API
       present User.all, with: API::Entities::User
     end
 
+    desc 'return a user'
+    params do
+      optional :id, type: Integer, desc: 'User id.'
+      optional :name, type: String, desc: 'User name.'
+      optional :email, type: String, desc: 'User email.'
+    end
+    get "info" do
+      present User.find_by(declared(params, include_missing: false)), with: API::Entities::User
+    end
+
   end
 end
