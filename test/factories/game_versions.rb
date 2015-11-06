4e554c64a1caf29f8199ee4fe53d11141f01cf74
Fan Jiang <fanjiang@thoughtworks.com>
2015-11-06 17:55:39 -0200

    range can't be used as sample

--- test/factories/game_versions.rb
+++ test/factories/game_versions.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :game_version do
     game nil
-    version { [0..3].sample }
-    language { [0..3].sample }
+    version { [0,1,2,3].sample }
+    language { [0,1,2,3].sample }
   end
 
 end
