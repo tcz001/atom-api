11ae11c17bfb2f80cfd70d8e0180e71b3ed7f688
Fan Jiang <fanjiang@thoughtworks.com>
2015-11-06 16:54:21 -0200

    hash key as integer

--- app/models/game_version.rb
+++ app/models/game_version.rb
@@ -3,24 +3,22 @@ class GameVersion < ActiveRecord::Base
   #TODO: 这里的i18n只是个例子，以后需要提供配置文件来加载
   @@i18n = {
       language: {
-          0: '英文版',
-          1: '日文版',
-          2: '中文版',
-          3: '香港繁体版',
+          0 => '英文版',
+          1 => '日文版',
+          2 => '中文版',
+          3 => '香港繁体版',
       },
       version: {
-          0: '港版',
-          1: '美版',
-          2: '日版',
-          3: '国行',
+          0 => '港版',
+          1 => '美版',
+          2 => '日版',
+          3 => '国行',
       },
-      
   }
   def display_language
     @@i18n[:language][self.language.to_sym]
   end
   def display_version
     @@i18n[:version][self.version.to_sym]
-    
   end
 end
