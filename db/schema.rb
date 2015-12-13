3de3fffc7354854084c41b468f107864c23bdef2
Fan Jiang <fanjiang@thoughtworks.com>
2015-12-13 11:14:41 -0500

    create new database for admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210215803) do
+ActiveRecord::Schema.define(version: 20151213161228) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "account",        limit: 255
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20151210215803) do
 
   add_index "accounts", ["game_id"], name: "index_accounts_on_game_id", using: :btree
   add_index "accounts", ["lease_order_id"], name: "index_accounts_on_lease_order_id", using: :btree
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.string   "password",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "charges", force: :cascade do |t|
     t.integer  "lease_order_id", limit: 4
---
+++ test/factories/admins.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :admin do
+    username "MyString"
+password "MyString"
+  end
+
+end
