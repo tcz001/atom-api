ecc8699353756991367d6528d016b3006e44bcac
Fan Jiang <fanjiang@thoughtworks.com>
2015-12-14 08:44:08 -0500

    expose more details in account

--- app/api/api/entities/account.rb
+++ app/api/api/entities/account.rb
@@ -2,8 +2,11 @@ module API
   module Entities
     class Account < Grape::Entity
       expose :game_id
+      expose :status
       expose :account
       expose :password
+      expose :start_at
+      expose :expire_at
     end
   end
 end
