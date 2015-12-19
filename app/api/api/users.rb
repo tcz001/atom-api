33b968bc5c7be756a8ff6af2dd22f7631df52f16
Fan Jiang <fanjiang@thoughtworks.com>
2015-12-19 16:14:56 -0500

    response token

--- app/api/api/users.rb
+++ app/api/api/users.rb
@@ -63,8 +63,11 @@ module API
           case user.status
             when 'confirmed'
               user.password = params[:password]
+              user.status = 'active'
               user.save
-              present user, with: API::Entities::User
+              access_token = Doorkeeper::AccessToken.create!(resource_owner_id: user.id, scopes: :public, expires_in: 3.days, use_refresh_token: true)
+              json = {token_info: access_token, access_token: access_token.token, refresh_token: access_token.refresh_token}.as_json
+              body json
             when 'confirming'
               error!({error: 'wait for confirming', detail: 'sign up error'}, 203)
             when 'active'
