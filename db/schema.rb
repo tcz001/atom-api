00826042e84247cb0f34f0c49d8729a9a216e1bb
Fan Jiang <fanjiang@thoughtworks.com>
2015-12-19 16:50:01 -0500

    use migration to remove restrict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213161228) do
+ActiveRecord::Schema.define(version: 20151219214901) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "account",        limit: 255
