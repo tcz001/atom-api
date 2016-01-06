31247f0d76fbed9777f3743a8130f7d95fe6bfd4
Fan Jiang <fanjiang@thoughtworks.com>
2016-01-06 18:00:25 -0500

    resize cover of games

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,9 +6,9 @@ class Game < ActiveRecord::Base
     self.game_type.name if self.game_type.present?
   end
   def cover
-    ApplicationController.helpers.qiniu_image_path(images.last.file.url, :thumbnail => '300x300', :quality => 80) if self.images.present?
+    ApplicationController.helpers.qiniu_image_path(images.last.file.url, :thumbnail => '500x500', :quality => 80) if self.images.present?
   end
   def cover_small
-    ApplicationController.helpers.qiniu_image_path(images.last.file.url, :thumbnail => '150x150', :quality => 50) if self.images.present?
+    ApplicationController.helpers.qiniu_image_path(images.last.file.url, :thumbnail => '250x250', :quality => 50) if self.images.present?
   end
 end
