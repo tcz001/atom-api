def5df1c1a58a0d4573dfa39d8b060648f98252d
Fan Jiang <fanjiang@thoughtworks.com>
2016-01-21 09:33:40 -0500

    hide detail from list

---
+++ app/api/api/entities/game_brief.rb
@@ -0,0 +1,20 @@
+module API
+  module Entities
+    class GameBrief < Grape::Entity
+      expose :id, as: :game_id
+      expose :name
+      expose :is_hot
+      expose :display_game_type, as: :game_type
+      expose :nick_name
+      expose :developer
+      expose :min_player_num
+      expose :max_player_num
+      expose :original_price
+      expose :reference_price
+      expose :cover
+      expose :cover_small
+      expose :display_language, as: :language
+      expose :release_at
+    end
+  end
+end
