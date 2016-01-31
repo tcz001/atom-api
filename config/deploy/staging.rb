ed189d18b150e8776de1c4eec6111f020f2d884b
Fan Jiang <fanjiang@thoughtworks.com>
2016-01-31 16:36:40 -0500

    update production config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'api.ecoolhub.com', user: 'atom-api', roles: %w{app web db}
+server 'test.api.ecoolhub.com', user: 'atom-api', roles: %w{app web db}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
