bf448ff6dbc53e7678d979bb4e3a43b343304622
Fan Jiang <fanjiang@thoughtworks.com>
2016-02-05 09:52:55 -0500

    change the period

--- app/api/api/lease_orders.rb
+++ app/api/api/lease_orders.rb
@@ -235,7 +235,7 @@ module API
           if lease_order.status == 2
             lease_order.accounts.each { |a|
               a.start_at = 1.day.from_now.beginning_of_day
-              a.expire_at = a.start_at + 7.days
+              a.expire_at = a.start_at + 6.days
               a.save
             }
             lease_order.status = 3
