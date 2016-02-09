f0830b0c6c90f23426f4515d51a45139139c3101
Fan Jiang <fanjiang@thoughtworks.com>
2016-02-08 19:16:05 -0500

    use master branch for staging

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ if [ "$ATOM_API_ENV" = "production" ]
 then
     /bin/bash --login -c "rvm use 2.2.1 && cap production deploy branch=release"
 else
-    /bin/bash --login -c "rvm use 2.2.1 && cap staging deploy branch=release"
+    /bin/bash --login -c "rvm use 2.2.1 && cap staging deploy branch=master"
 fi
