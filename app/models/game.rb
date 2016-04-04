50cab8de06950854f951dc7c2b02cb80d50da9e2
Fan Jiang <fanjiang@thoughtworks.com>
2016-04-04 14:52:45 -0500

    ignore invalid skus

--- app/models/game.rb
+++ app/models/game.rb
@@ -19,12 +19,12 @@ class Game < ActiveRecord::Base
     self.language.split(',').map { |l| @@i18n[:language][l] }.join(',') if self.language.present?
   end
   def display_reference_price
-    sorted_prices = self.game_skus.sort_by{|e| e[:price]}
+    sorted_prices = self.game_skus_published.sort_by{|e| e[:price]}
     sorted_prices.first.price if sorted_prices.present?
   end
   def price_range
     if self.game_skus.present?
-      sorted_prices = self.game_skus.sort_by{|e| e[:price]}
+      sorted_prices = self.game_skus_published.sort_by{|e| e[:price]}
       return "#{sorted_prices.first.price}-#{sorted_prices.last.price}"
     end
   end
